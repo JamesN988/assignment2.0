3bc51da031a8f6e9faa4968bd5a06abf1e5d5758
James <nguyenjames998@gmail.com>
2025-05-03 16:59:44 +0930

    Next i added checksum and corruption detection functions after analysing how the gbn worked with the emulator

--- sr.c
+++ sr.c
@@ -9,4 +9,13 @@
 #define SEQSPACE 12
 #define NOTINUSE (-1)
 
+int ComputeChecksum(struct pkt packet) {
+  int checksum = packet.seqnum + packet.acknum;
+  for (int i = 0; i < 20; i++)
+      checksum += packet.payload[i];
+  return checksum;
+}
 
+bool IsCorrupted(struct pkt packet) {
+  return packet.checksum != ComputeChecksum(packet);
+}
