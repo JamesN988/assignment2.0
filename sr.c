4851acc269e6f64f46caa57b68831a395e582d98
James <nguyenjames998@gmail.com>
2025-05-03 17:01:08 +0930

    Implemented A_output to send packets and store inside an array. Also starting the timer

--- sr.c
+++ sr.c
@@ -29,3 +29,31 @@ struct sender_packet {
 static struct sender_packet A_buffer[SEQSPACE];
 static int A_base = 0;
 static int A_nextseq = 0;
+
+void A_output(struct msg message) {
+  if (((A_nextseq - A_base + SEQSPACE) % SEQSPACE) < WINDOWSIZE) {
+      struct pkt pkt;
+      pkt.seqnum = A_nextseq;
+      pkt.acknum = NOTINUSE;
+
+      for (int i = 0; i < 20; i++)
+          pkt.payload[i] = message.data[i];
+
+      pkt.checksum = ComputeChecksum(pkt);
+      A_buffer[A_nextseq].packet = pkt;
+      A_buffer[A_nextseq].acked = false;
+      A_buffer[A_nextseq].sent = true;
+
+      tolayer3(A, pkt);
+      starttimer(A, RTT);
+
+      if (TRACE > 1)
+          printf("A_output: Sent packet %d\n", pkt.seqnum);
+
+      A_nextseq = (A_nextseq + 1) % SEQSPACE;
+  } else {
+      if (TRACE > 0)
+          printf("A_output: Window full, message dropped\n");
+      window_full++;
+  }
+}
